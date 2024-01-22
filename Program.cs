0a20e0d987eccee44a444d085c3206dde4008498
Um6253 <um6253@srmist.edu.in>
2024-01-22 14:41:43 +0530

    [Mani]: Adding branches

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_And_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inital_positon = 0;
+        }
+    }
+}
