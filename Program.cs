e455fce42b7015a236d581192895aadf9fd6a3b8
Um6253 <um6253@srmist.edu.in>
2024-01-22 14:55:20 +0530

    [Mani]: Adding branche UC3

--- Program.cs
+++ Program.cs
@@ -1,19 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
+using System;
 
-namespace Snake_And_Ladder
+namespace snake_and_ladder
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Random rnm = new Random();
-            int random = rnm.Next(6);
-            Console.WriteLine(random);
+            int player1 = 0;
+            Random rand = new Random();
+            int random_number = rand.Next(6);
+            random_number++;
+            Console.WriteLine($"The number on the dice is {random_number}");
+            int option = rand.Next(3);
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("No move");
+                    break;
+                case 1:
+                    Console.WriteLine("It is a ladder");
+                    player1 += random_number;
+                    break;
+                case 2:
+                    Console.WriteLine("It is a snake");
+                    if (random_number > player1)
+                    {
+                        player1 = 0;
+                    }
+                    else
+                    {
+                        player1 -= random_number;
+                    }
+                    break;
+            }
+            Console.Write("player postiton is : ");
+            Console.WriteLine(player1);
             Console.ReadLine();
+
 
 
         }
